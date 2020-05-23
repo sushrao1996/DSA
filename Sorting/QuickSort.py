46650377fa7e61fc11407cdc31206ee05cc86434
Naga Sushmitha Mavilla <sushrao1996@gmail.com>
2020-05-23 22:45:32 +0530

    Add files via upload

---
+++ Sorting/QuickSort.py
@@ -0,0 +1,20 @@
+def helper(arr,low,high):
+    pivot_index=arr[high]
+    i=low-1
+    for j in range(low,high):
+        if arr[j]<=pivot_index:
+            i=i+1
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+    return i+1
+def quicksort(arr,low,high):
+    if low<high:
+        pi=helper(arr,low,high)
+        quicksort(arr,low,pi-1)
+        quicksort(arr,pi+1,high)
+arr=[5,7,6,9,4,8,1,2,3]
+n=len(arr)
+quicksort(arr,0,n-1)
+
+for i in arr:
+    print(i,end=" ")
